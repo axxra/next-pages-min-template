16f6890c93d73ee4a35d66866aa4f8daa1c8a0b7
axxra <144186316+axxra@users.noreply.github.com>
2024-05-11 01:18:28 +0530

    Dynamic Color

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import "@mantine/core/styles.css";
 import Head from "next/head";
-import { MantineColorsTuple, MantineProvider, createTheme } from "@mantine/core";
+import { MantineColorsTuple, MantineProvider, createTheme, virtualColor } from "@mantine/core";
 //https://tabler.io/icons
 
 export default function App({ Component, pageProps }: any) {
@@ -17,16 +17,48 @@ export default function App({ Component, pageProps }: any) {
     "#267c9a",
     "#016c89"
   ];
+  const myLight: MantineColorsTuple = [
+    "#e5f9ff",
+    "#d6edf6",
+    "#b2d8e7",
+    "#8bc3d9",
+    "#6ab0cc",
+    "#54a5c5",
+    "#469fc2",
+    "#348aac",
+    "#267c9a",
+    "#016c89"
+  ];
+  const myDark: MantineColorsTuple = [
+    "#f7f3f2",
+    "#e7e5e5",
+    "#d2c9c6",
+    "#bdaaa4",
+    "#ab9087",
+    "#a17f75",
+    "#9d766b",
+    "#896459",
+    "#7b584e",
+    "#6d4b40"
+  ];
 
   const theme = createTheme({
     colors: {
       myColor,
+      myDark,
+      myLight,
+      myPrimary: virtualColor({
+        name: 'myPrimary',
+        dark: 'myDark',
+        light: 'myLight',
+      }),
     },
-    primaryColor: "myColor"
+    primaryColor: "myPrimary",
+    defaultRadius: 15,
   });
 
   return (
-    <MantineProvider defaultColorScheme="dark" theme={theme}>
+    <MantineProvider defaultColorScheme="auto" theme={theme}>
       <Head>
         <title>Mantine Template</title>
         <meta
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
-import { Button, Group, rem, useComputedColorScheme, useMantineColorScheme } from "@mantine/core";
-import { IconSun } from "@tabler/icons-react";
+import { ActionIcon, Button, Group, rem, useComputedColorScheme, useMantineColorScheme } from "@mantine/core";
+import { IconMoon, IconSun } from "@tabler/icons-react";
 
 export default function IndexPage() {
 
@@ -9,9 +9,22 @@ export default function IndexPage() {
   return (
     <Group pt={50} justify="center" align="center" style={{ height: '100vh', display: "flex", flexDirection: "column" }}>
       <Button /* color="myColor"  */ size="xl">Welcome to Mantine!</Button>
-      <Button mt={10} leftSection={<IconSun style={{ width: rem(20), height: rem(20) }} stroke={3} />} size="md" onClick={() => {
+      <Button mt={10} leftSection={computedColorScheme === 'dark' ? <IconSun style={{ width: rem(22), height: rem(22) }} stroke={1.5} /> : <IconMoon style={{ width: rem(22), height: rem(22) }} stroke={1.5} />} size="md" onClick={() => {
         setColorScheme(computedColorScheme === 'dark' ? 'light' : 'dark');
-      }}>Change Theme</Button>
+      }}>{computedColorScheme === 'dark' ? `Light` : `Dark`} Theme</Button>
+      <ActionIcon
+        mt={10}
+        onClick={() => setColorScheme(colorScheme === 'light' ? 'dark' : 'light')}
+        variant="default"
+        size="xl"
+        aria-label="Toggle color scheme"
+      >
+        {
+          computedColorScheme === 'dark' ?
+            <IconSun stroke={1.5} style={{ width: rem(22), height: rem(22) }} /> :
+            <IconMoon stroke={1.5} style={{ width: rem(22), height: rem(22) }} />
+        }
+      </ActionIcon>
     </Group>
   );
 }
